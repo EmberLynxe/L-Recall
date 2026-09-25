af5b30141c175c27759086392c59580e64215e95
EmberLynxe <333665730+EmberLynxe@users.noreply.github.com>
2026-09-26 00:05:10 +1000

    tray click opens the window

--- league_vcs/gui/tray_item.py
+++ league_vcs/gui/tray_item.py
@@ -18,14 +18,16 @@ class TrayItem(wx.adv.TaskBarIcon):
     ICON_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'icons', 'small-icon.png'))
     TOOLTIP = 'L-Recall'
 
-    def __init__(self, menu_options, on_left_click=False):
+    def __init__(self, menu_options, on_click=None):
         wx.adv.TaskBarIcon.__init__(self)
         self.set_icon()
 
         self.menu_options = menu_options
-        self.on_left_click = on_left_click
+        self.on_click = on_click
 
-        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+        # used to run whatever was first in the menu, which turned into "download update" once there was one
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_UP, self.on_left_up)
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DCLICK, self.on_left_up)
 
     def CreatePopupMenu(self):
         menu = wx.Menu()
@@ -46,8 +48,6 @@ class TrayItem(wx.adv.TaskBarIcon):
         icon = wx.Icon(wx.Bitmap(self.ICON_PATH, type=wx.BITMAP_TYPE_PNG))
         self.SetIcon(icon, self.TOOLTIP)
 
-    def on_left_down(self, _):
-        """Tray icon was left-clicked, trigger the required action."""
-        if self.on_left_click is False:
-            return
-        self.menu_options[self.on_left_click][1]()
+    def on_left_up(self, _):
+        if self.on_click:
+            self.on_click()
