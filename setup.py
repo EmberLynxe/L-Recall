78f80f5c3b7c47e00f3e665f58c19171a3787364
EmberLynxe <333665730+EmberLynxe@users.noreply.github.com>
2026-09-26 18:08:39 +1000

    1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='l-recall',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     author='EmberLynxe',
     entry_points={
